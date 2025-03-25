81d1cef2cf87fecb914aff9afe1c8027f9cfa956
EfrenDaMo <david.mora.work@gmail.com>
2025-03-24 23:06:03 -0600

    Se agrego templetes, icono nuevo y logica de ruteo basica

--- src/app.py
+++ src/app.py
@@ -1,1 +1,22 @@
-print("Hola, mundo")
+from flask import Flask, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return redirect(url_for("login"))
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", titulo="Login")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", titulo="Dashboard")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
