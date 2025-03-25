c3747bb3d83d468bcc7e36c33a15309e0ced5324
EfrenDaMo <david.mora.work@gmail.com>
2025-03-25 12:33:42 -0600

    Se termino el modulo de base de datos implementando methodos basicos CRUD

--- src/modules/database.py
+++ src/modules/database.py
@@ -36,11 +36,15 @@ class BasedeDatos:
             self.conexion.close()
             self.conexion = None
 
-    def ejecutar_consulta(self, consulta: str, parametros: list[str] | None = None):
+    def __ejecutar_consulta(
+        self,
+        consulta: str,
+        parametros: list[str] | None = None,
+    ):
         """
         Metodo para ejecutar consultas SQL
         :param consulta: La consulta que se ejecutara
-        :param parametros: Parametros de la consulta (Opcional)
+        :param params: Parametros de la consulta (Opcional)
         """
         if not self.conexion:
             self.conectar()
@@ -51,10 +55,112 @@ class BasedeDatos:
                     self.cursor.execute(consulta, parametros)
                 else:
                     self.cursor.execute(consulta)
-
         except mysql.connector.Error as err:
             print(f"Error ejecutando consulta: {err}")
             raise
+
+    def crear(self, tabla: str, datos: dict[str, str]):
+        """
+        Crear un nuevo registro
+
+        :param tabla: Nombre de la tabla
+        :param datos: Dictionario con pares de campo:valor
+        """
+        valores = list(datos.values())
+        campos = ",".join(datos.keys())
+        marcadores = ", ".join(["%s"] * len(datos))
+
+        consulta = f"INSERT INTO `{tabla}` ({campos}) VALUES ({marcadores}])"
+
+        try:
+            self.__ejecutar_consulta(consulta, valores)
+            self.confirmar()
+        except Exception as err:
+            self.revertir()
+            raise err
+
+    def leer(
+        self,
+        tabla: str,
+        condiciones: dict[str, str] | None = None,
+        campos: list[str] | str = "*",
+    ):
+        """
+        Leer registro de la tabla
+
+        :param tabla: Nombre de la tabla
+        :param condiciones: Dictionario opcional que contiene las condiciones de lectura
+        :param campos: Campos a escoger, se escogen todos por defecto
+        :return: Lista de los registros
+        """
+        if isinstance(campos, list):
+            campos = ", ".join(campos)
+
+        if condiciones:
+            clausula_where = " AND ".join(
+                [f"`{campo}`=%s" for campo in condiciones.keys()]
+            )
+            consulta = f"SELECT {campos} FROM {tabla} WHERE {clausula_where}"
+        else:
+            consulta = f"SELECT {campos} FROM {tabla}"
+
+        self.__ejecutar_consulta(consulta)
+        if self.cursor:
+            return self.cursor.fetchall()
+
+    def actualizar(
+        self,
+        tabla: str,
+        datos: dict[str, str],
+        condiciones: dict[str, str],
+    ):
+        """
+        Actualizar registro
+
+        :param tabla: Nombre de la tabla
+        :param datos: Dictionario de campos a actualizar
+        :param condiciones: Dictionario que contiene las condiciones de actualizacion
+        :return: Numero de registros actualizados
+        """
+        clausula_set = ", ".join([f"`{campo}`=%s" for campo in datos.keys()])
+        clausula_where = " AND ".join([f"`{campo}`=%s" for campo in condiciones.keys()])
+
+        valores = list(datos.values()) + list(condiciones.values())
+
+        consulta = f"UPDATE {tabla} SET {clausula_set} WHERE {clausula_where}"
+
+        try:
+            self.__ejecutar_consulta(consulta, valores)
+            self.confirmar()
+
+            if self.cursor:
+                return self.cursor.rowcount
+        except Exception as err:
+            self.revertir()
+            raise err
+
+    def eliminar(self, tabla: str, condiciones: dict[str, str]):
+        """
+        Eliminar registro
+
+        :param tabla: Nombre de la tabla
+        :param condiciones: Dictionario que contiene las condiciones de eliminado
+        :return: Numero de registros eliminados o un error en caso de falla
+        """
+        clausula_where = " AND ".join([f"`{campo}`=%s" for campo in condiciones.keys()])
+
+        consulta = f"DELETE FROM {tabla} WHERE {clausula_where}"
+        valores = list(condiciones.values())
+
+        try:
+            self.__ejecutar_consulta(consulta, valores)
+            self.confirmar()
+
+            if self.cursor:
+                return self.cursor.rowcount
+        except Exception as err:
+            self.revertir()
+            raise err
 
     def confirmar(self):
         """Confirma la transaction actual"""
